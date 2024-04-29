96edbb4fbc6f86a6c65d849f05ef384595020dac
Nisha7781 <nishamali0781@gmail.com>
2024-04-29 12:09:19 +0530

    Master Created

---
+++ src/main/java/org/example/DBdemo.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.sql.*;
+
+public class DBdemo {
+    public static void main(String[] args) {
+        System.out.println("Employee Payroll using java JDBC");
+    }
+}
